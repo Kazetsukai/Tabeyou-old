ceeda9eee1116624d8150bea2c4734ff32bf3db9
Daniel Tebbutt <moeruze.burning@gmail.com>
2015-06-06 19:17:43 +1200

    Trying to get relative hostname

--- Foundation.hs
+++ Foundation.hs
@@ -52,7 +52,7 @@ type Form x = Html -> MForm (HandlerT App IO) (FormResult x, Widget)
 -- Please see the documentation for the Yesod typeclass. There are a number
 -- of settings which can be configured by overriding methods here.
 instance Yesod App where
-    approot = ApprootMaster $ appRoot . settings
+    approot = ApprootRelative
 
     -- Store session data on the client in encrypted cookies,
     -- default session idle timeout is 120 minutes
